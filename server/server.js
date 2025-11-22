bc5a9d590bcea59430891a25db166daaa973e4f7
Sanjai-raj <142824931+Sanjai-raj@users.noreply.github.com>
2025-11-22 12:33:29 +0530

    Server updation and seed updation

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ const Car = require('./models/Car');
 const Booking = require('./models/Booking');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: [
+      "https://crosscity-rentals5.onrender.com",
+      "http://localhost:5173"
+    ],
+    credentials: true,
+  }));
 app.use(express.json());
 
 // --- STRICT MONGO CONNECTION ---
